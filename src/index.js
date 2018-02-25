95ad7d7087492d038544586f57dfe7d3a3f7a4ed
UnDragon <31067730+UnDragon@users.noreply.github.com>
2018-02-25 18:01:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency > 10000){
+        return   {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    }
+    var coins = {};
+    function subtraction(value , letter) {
+        while (currency >= value){
+            currency -= value;
+            if (coins[letter]){
+                coins[letter]++;
+            }
+            else {
+                coins[letter] = 1;
+            }
+        }
+    }
+    subtraction(50, "H");
+    subtraction(25, "Q");
+    subtraction(10, "D");
+    subtraction(5, "N");
+    subtraction(1, "P");
+    return coins;
 }
